b53718383363b63a60f053394e4b53f528f4cbe6
Igor Krivoruchko <kigoranatolevich@gmail.com>
2023-06-17 16:48:48 +0300

    Реализация LoadBalancer в API Gateway

--- eureka-client/src/main/java/com/javabegin/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/javabegin/micro/demo/eurekaclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.javabegin.micro.demo.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,12 @@ import javax.swing.*;
 @RequestMapping("/main")
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/test")
     public String test() {
-        return "test";
+        // id будет показывать нам какое приложение выполнено
+        return id;
     }
 }
