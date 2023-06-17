1ea30ad1d8985ce8d35891701f156a04a2632ba4
Igor Krivoruchko <kigoranatolevich@gmail.com>
2023-06-17 16:04:38 +0300

    Перенаправление запроса на нужный микросервис. Создание модуля eureka-client-2

--- eureka-client/src/main/java/com/javabegin/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/javabegin/micro/demo/eurekaclient/controller/TestController.java
@@ -1,11 +1,13 @@
 package com.javabegin.micro.demo.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.swing.*;
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
 
     @GetMapping("/test")
