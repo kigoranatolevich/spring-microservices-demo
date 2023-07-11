d6ca95e82f01a50b98436a6585d335a9b846df04
Igor Krivoruchko <kigoranatolevich@gmail.com>
2023-07-11 17:59:38 +0300

    сÐоздание new-branch

---
+++ test-git/new-branch/src/main/java/com/example/newbranch/NewBranchApplication.java
@@ -0,0 +1,13 @@
+package com.example.newbranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewBranchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewBranchApplication.class, args);
+    }
+
+}
---
+++ test-git/new-branch/src/test/java/com/example/newbranch/NewBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newbranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewBranchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
